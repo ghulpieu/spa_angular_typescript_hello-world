07e9252d86e67eae96e89f6cbbb419a6b62ebc17
Gordon Hulpieu <ghulpieu@gmail.com>
2024-12-09 17:33:43 -0500

    feat: create Login, Logout, and SignUp components
      On branch starter
      Your branch is ahead of 'origin/starter' by 1 commit.
        (use "git push" to publish your local commits)
    
      Changes to be committed:
     	new file:   src/app/shared/components/buttons/login-button.component.ts
     	new file:   src/app/shared/components/buttons/logout-button.component.ts
     	new file:   src/app/shared/components/buttons/signup-button.component.ts
     	modified:   src/app/shared/shared.module.ts

---
+++ src/app/shared/components/buttons/logout-button.component.ts
@@ -0,0 +1,22 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-logout-button',
+  template: `
+    <button class="button__logout" (click)="handleLogout()">Log Out</button>
+  `,
+})
+export class LogoutButtonComponent {
+  private auth = inject(AuthService);
+  private doc = inject(DOCUMENT);
+
+  handleLogout(): void {
+    this.auth.logout({
+      logoutParams: {
+        returnTo: this.doc.location.origin,
+      },
+    });
+  }
+}
