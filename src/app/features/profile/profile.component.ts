1f8f3dc5af6d77dbe3ecebe9c534f00150984bc8
Gordon Hulpieu <ghulpieu@gmail.com>
2024-12-11 11:32:55 -0500

    feat: get user profile information in Angular
      On branch starter
      Your branch is ahead of 'origin/starter' by 3 commits.
        (use "git push" to publish your local commits)
    
      Changes to be committed:
     	modified:   src/app/features/profile/profile.component.ts

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,24 +1,18 @@
-import { Component } from '@angular/core';
-import { of } from 'rxjs';
+/* eslint-disable @typescript-eslint/member-ordering */
+
+import { Component, inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 import { map } from 'rxjs/operators';
-import { UserProfileModel } from '@app/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent {
-  user$ = of({
-    nickname: 'Customer',
-    name: 'One Customer',
-    picture: 'https://cdn.auth0.com/blog/hello-auth0/auth0-user.png',
-    updated_at: '2021-05-04T21:33:09.415Z',
-    email: 'customer@example.com',
-    email_verified: false,
-    sub: 'auth0|12345678901234567890',
-  } as UserProfileModel);
+  private auth = inject(AuthService);
 
-  title = 'User Profile Object';
+  title = 'Decoded ID Token';
 
+  user$ = this.auth.user$;
   code$ = this.user$.pipe(map((user) => JSON.stringify(user, null, 2)));
 }
