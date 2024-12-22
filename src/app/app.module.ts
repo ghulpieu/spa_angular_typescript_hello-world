283a980d9be8e0669bc0bd607344909d05786ec9
Gordon Hulpieu <ghulpieu@gmail.com>
2024-12-22 15:33:50 -0500

    chore: render the application conditionally
      On branch starter
      Your branch is up to date with 'origin/starter'.
    
      Changes to be committed:
     	modified:   src/app/app.component.html
     	modified:   src/app/app.component.ts
     	modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from '@auth0/auth0-angular';
 import { environment as env } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared';
 
 @NgModule({
     declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
         AuthModule.forRoot({
           ...env.auth0,
         }),
+        SharedModule,
     ],
     providers: [provideHttpClient()] })
 export class AppModule {}
