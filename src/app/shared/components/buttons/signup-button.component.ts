fda917b9d1b5ea3891164faecd839cb3f4a0e5e9
Gordon Hulpieu <ghulpieu@gmail.com>
2024-12-22 12:18:35 -0500

    fix: typo in signUp component template
      On branch starter
      Your branch is ahead of 'origin/starter' by 1 commit.
        (use "git push" to publish your local commits)
    
      Changes to be committed:
     	modified:   src/app/shared/components/buttons/signup-button.component.ts

--- src/app/shared/components/buttons/signup-button.component.ts
+++ src/app/shared/components/buttons/signup-button.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-signup-button',
   template: `
-    <button class="button__sign-up" (click)="handleSignUp">Sign Up</button>
+    <button class="button__sign-up" (click)="handleSignUp()">Sign Up</button>
 `
 })
 export class SignupButtonComponent {
