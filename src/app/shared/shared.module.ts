21550fbbf0f50eef97742f8d0fb46540638a3a7f
Gordon Hulpieu <ghulpieu@gmail.com>
2024-12-22 12:26:19 -0500

    chore: add user Logout to Angular
      On branch starter
      Your branch is ahead of 'origin/starter' by 2 commits.
        (use "git push" to publish your local commits)
    
      Changes to be committed:
     	new file:   src/app/shared/components/buttons/logout-button.component.ts
     	modified:   src/app/shared/shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,18 +4,21 @@ import { RouterModule } from '@angular/router';
 import { COMPONENTS } from './components';
 import { LoginButtonComponent } from './components/buttons/login-button.component';
 import { SignupButtonComponent } from './components/buttons/signup-button.component';
+import { LogoutButtonComponent } from './components/buttons/logout-button.component';
 
 @NgModule({
   declarations: [
     ...COMPONENTS,
     LoginButtonComponent,
-    SignupButtonComponent
+    SignupButtonComponent,
+    LogoutButtonComponent
   ],
   imports: [CommonModule, RouterModule],
   exports: [
     ...COMPONENTS,
     LoginButtonComponent,
-    SignupButtonComponent
+    SignupButtonComponent,
+    LogoutButtonComponent
   ],
 })
 export class SharedModule {}
